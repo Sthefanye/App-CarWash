0ec40bf6c33b51fe2f51eab82c8635cef3ab2dbb
Jose_Ribamar <jrqdsn.hef4@uea.edu.br>
2021-10-03 14:33:32 -0400

    Consertando localização das pastas e adicionando as pastas ui/activities e view

--- app/src/androidTest/java/com/example/lavajato/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lavajato/ExampleInstrumentedTest.kt
@@ -22,3 +22,18 @@ class ExampleInstrumentedTest {
         assertEquals("com.example.lavajato", appContext.packageName)
     }
 }
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.myapplication", appContext.packageName)
+    }
+}
--- app/src/main/java/com/example/lavajato/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/lavajato/ui/activities/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myapplication
+package com.example.lavajato.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
