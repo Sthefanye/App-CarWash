3c4b267328df1454b3ef5fa2dfa9fb8764fe5a38
queirozsnr <queirozsnr@gmail.com>
2021-10-27 20:47:02 -0400

    Configuração inicial firebase

--- app/src/main/java/com/example/carwash/ui/LoginActivity.kt
+++ app/src/main/java/com/example/carwash/ui/LoginActivity.kt
@@ -3,10 +3,18 @@ package com.example.carwash.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.carwash.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
-    }
-}
+
+        database = Firebase.database.reference//ooo
+
+        database.child("pontos").setValue("oi")
+    }}
