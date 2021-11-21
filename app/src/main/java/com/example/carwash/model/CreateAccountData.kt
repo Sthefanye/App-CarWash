060a8979e6b76d1ce9d2745fdf048bc557748f1d
Leonardo Valcacio <leonardo.valcacio@itriad.org.br>
2021-11-20 23:44:30 -0400

    Add layout da tela home

--- app/src/main/java/com/example/carwash/model/CreateAccountData.kt
+++ app/src/main/java/com/example/carwash/model/CreateAccountData.kt
@@ -1,10 +1,34 @@
 package com.example.carwash.model
 
 import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import com.example.carwash.BR
 
 class CreateAccountData : BaseObservable() {
+    @Bindable
     var name : String = ""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.name)
+    }
+
+    @Bindable
     var email : String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.email)
+        }
+    @Bindable
     var password : String = ""
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.password)
+        }
+
+    @Bindable
     var telephone : Int = 0
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.telephone)
+        }
 }
---
+++ app/src/main/java/com/example/carwash/ui/bindings/HomeFragmentFormatter.kt
@@ -0,0 +1,9 @@
+package com.example.carwash.ui.bindings
+
+object HomeFragmentFormatter {
+
+    @JvmStatic
+    fun formatNameUser(name: String) : String {
+        return "Olá, $name"
+    }
+}
