05736db010980952868b90adc64f6aeabe8883d0
queirozsnr <queirozsnr@gmail.com>
2021-11-29 19:19:26 -0400

    configuracoes iniciais do gradle para a sincronizao do banco de dados

--- app/src/main/java/com/example/carwash/ui/MainActivity.kt
+++ app/src/main/java/com/example/carwash/ui/MainActivity.kt
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.example.carwash.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var FirebaseDatabase = FirebaseDatabase.getInstance()
+        var DatabaseReference = FirebaseDatabase.getReference()
+        DatabaseReference.child("teste").push().setValue("100")
     }
 }
