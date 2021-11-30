aee78b6bb6977646080670b95d95714eee4ebf0b
Leonardo Valcacio <leonardo.valcacio@itriad.org.br>
2021-11-29 20:57:43 -0400

    roando o firebase

--- app/src/main/java/com/example/carwash/ui/MainActivity.kt
+++ app/src/main/java/com/example/carwash/ui/MainActivity.kt
@@ -2,17 +2,15 @@ package com.example.carwash.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.example.carwash.R
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var FirebaseDatabase = FirebaseDatabase.getInstance()
-        var DatabaseReference = FirebaseDatabase.getReference()
-        DatabaseReference.child("teste").push().setValue("100")
+        val firebaseDatabase = FirebaseDatabase.getInstance()
+        val databaseReference = firebaseDatabase.reference
+        databaseReference.child("teste").push().setValue("100")
     }
 }
