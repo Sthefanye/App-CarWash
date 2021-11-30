7ad2f9fa7b053e7b1baabb2303797a7af1f3adc3
Sthe <66852308+Sthefanye@users.noreply.github.com>
2021-11-29 21:00:37 -0400

    Subindo o firebase (#14)
    * configuracoes iniciais do gradle para a sincronizao do banco de dados
    
    * removendo o formatter do fragment_home.xml
    
    * removendo o formatter do fragment_home.xml
    
    * roando o firebase
    
    Co-authored-by: queirozsnr <queirozsnr@gmail.com>
    Co-authored-by: Leonardo Valcacio <leonardo.valcacio@itriad.org.br>

--- app/src/main/java/com/example/carwash/ui/MainActivity.kt
+++ app/src/main/java/com/example/carwash/ui/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.carwash.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.carwash.R
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firebaseDatabase = FirebaseDatabase.getInstance()
+        val databaseReference = firebaseDatabase.reference
+        databaseReference.child("teste").push().setValue("100")
     }
 }
 
