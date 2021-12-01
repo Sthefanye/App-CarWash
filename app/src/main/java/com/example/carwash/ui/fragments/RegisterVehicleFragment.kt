0bc53e1d6598d5521a40334791f5911ad13045c8
Sthe <66852308+Sthefanye@users.noreply.github.com>
2021-11-30 20:03:18 -0400

    Navigation (#15)
    * atualizando home fragment
    
    * Add navigation
    
    * edit gitignore
    
    * delete .idea
    
    Co-authored-by: Leonardo Valcacio <leonardo.valcacio@itriad.org.br>

--- app/src/main/java/com/example/carwash/ui/fragments/RegisterVehicleFragment.kt
+++ app/src/main/java/com/example/carwash/ui/fragments/RegisterVehicleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.carwash.R
 import com.example.carwash.databinding.FragmentListaServicosBinding
 import com.example.carwash.databinding.FragmentRegisterVehicleBinding
@@ -25,7 +26,22 @@ class RegisterVehicleFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View {
         registerVeiculoBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_register_vehicle, container, false)
+        navigateToHome()
+        navigateToMeusVeiculos()
+
         return registerVeiculoBinding.root
+    }
+
+    private fun navigateToHome() {
+        registerVeiculoBinding.btnVoltarHome.setOnClickListener {
+            findNavController().navigate(R.id.nav_meus_veiculos_to_home)
+        }
+    }
+
+    private fun navigateToMeusVeiculos() {
+        registerVeiculoBinding.btnConfirmRegisterVeiculo.setOnClickListener {
+            findNavController().navigate(R.id.nav_meus_veiculos_to_register_veiculos)
+        }
     }
 
 }
--- app/src/main/java/com/example/carwash/ui/fragments/StatusFragment.kt
+++ app/src/main/java/com/example/carwash/ui/fragments/StatusFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.carwash.R
 import com.example.carwash.databinding.FragmentStatusBinding
 
@@ -25,7 +26,13 @@ class StatusFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         statusBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_status, container, false)
+        navigateStatusToHome()
         return statusBinding.root
     }
 
+    private fun navigateStatusToHome() {
+        statusBinding.btnVoltar.setOnClickListener {
+            findNavController().navigate(R.id.nav_status_to_home)
+        }
+    }
 }
